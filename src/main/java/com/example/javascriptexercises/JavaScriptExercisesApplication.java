bcd6f52d5febc06cae0c6e2b672e604be7e88114
Troels <troe1912@stud.kea.dk>
2022-09-20 13:55:35 +0200

    init commit

---
+++ src/main/java/com/example/javascriptexercises/JavaScriptExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.javascriptexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScriptExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaScriptExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javascriptexercises/JavaScriptExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javascriptexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScriptExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
